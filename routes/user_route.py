dc13614e2b1f1e2aa2afeded73867e918b6f9cd3
Ktulha <9145140@gmail.com>
2024-10-28 18:00:04 +0300

    up/ bootstrap and fontawesome

--- routes/user_route.py
+++ routes/user_route.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template
 from sqlalchemy import desc, func
 
-from database.database import Book
+from database.database import Book, Genre
 
 user_route=Blueprint('user_route',__name__,template_folder='../templates/')
 
@@ -11,3 +11,12 @@ def index():
   books = Book.query.order_by(desc(Book.id)).limit(15).all()
   return render_template('index.html', books=books)
 
+@user_route.route('/Books')
+def get_books():
+  books = Book.query.all()
+  return render_template('Books.html', books=books,active_page='books')
+
+@user_route.route('/Genres')
+def get_genres():
+  genres = Genre.query.all()
+  return render_template('Genres.html', genres=genres,active_page='genres')
